241c488c058c8509757abc0c82d2e7785ee4749c
aldy505 <reinaldyrafli@gmail.com>
2021-02-16 12:50:39 +0700

    Initial nodejs version

---
+++ test/main.js
@@ -0,0 +1,13 @@
+require('mocha')
+
+describe('main test', () => {
+    it('should output an image', async () => {
+        //
+    })
+    it('should return error', async () => {
+        //
+    })
+    it('should return too many request', async () => {
+        //
+    })
+})
