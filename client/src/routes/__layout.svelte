7829bb2fdfda8a67758ed42c8b79d28030052b48
Reinaldy Rafli <aldy505@tutanota.com>
2021-07-16 15:15:28 +0700

    feat: language switcher

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -5,6 +5,7 @@
 <script lang="ts">
   import '@fontsource/fira-mono';
   import '@fontsource/rubik';
+  import { goto } from '$app/navigation'
   import { _ } from 'svelte-i18n';
   import Navbar from '../components/navbar.svelte';
 </script>
@@ -34,11 +35,19 @@
     </main>
 
     <footer class="font-body py-8">
-      <p class="text-sm opacity-50 hover:opacity-90 transition duration-300 ease-in-out">
+      <p class="text-sm opacity-50 hover:opacity-90 transition duration-300 ease-in-out inline-block">
         {$_('footer.made')} <span class="text-red-500">&#10084;</span>
         {$_('footer.indonesia')}.
         {$_('footer.available')}
         <a href="https://www.github.com/aldy505/jokes-bapak2-api" class="hover:underline">{$_('footer.github')}</a>.
+      </p>
+      <p class="text-sm opacity-50 inline-block">
+        &nbsp;|&nbsp;
+      </p>
+      <p class="text-sm opacity-50 hover:opacity-90 transition duration-300 ease-in-out inline-block">
+        {$_('footer.language')}
+        <a href="?lang=en" target="_top" class="hover:underline">{$_('footer.english')}</a>
+        <a href="?lang=id" target="_top" class="hover:underline">{$_('footer.indonesian')}</a>
       </p>
     </footer>
   </div>
