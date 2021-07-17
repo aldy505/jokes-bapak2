c490c75a5e319b706a5016924ea9e0eca3c220ff
Reinaldy Rafli <aldy505@tutanota.com>
2021-07-17 20:20:51 +0700

    fix: seo problem with one small change

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -16,7 +16,6 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    ssr: false,
     trailingSlash: 'never',
     files: {
       routes: './src/routes',
