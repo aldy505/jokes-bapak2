2237bd7efdff106a08af92650e11fad7d170dcc9
Reinaldy Rafli <aldy505@tutanota.com>
2021-07-23 22:02:39 +0700

    refactor: updated bob & refactored create table sql

--- api/app/v1/platform/database/create.go
+++ api/app/v1/platform/database/create.go
@@ -11,11 +11,43 @@ import (
 func Setup() error {
 	db := New()
 
+	// administrators table
+	var tableAuthExists bool
+	err := db.QueryRow(context.Background(), `SELECT EXISTS (
+		SELECT FROM information_schema.tables 
+		WHERE  table_schema = 'public'
+		AND    table_name   = 'administrators'
+		);`).Scan(&tableAuthExists)
+	if err != nil {
+		log.Fatalln("16 - failed on checking table: ", err)
+		return err
+	}
+
+	if !tableAuthExists {
+		sql, _, err := bob.
+			CreateTable("administrators").
+			AddColumn(bob.ColumnDef{Name: "id", Type: "SERIAL", Extras: []string{"PRIMARY KEY"}}).
+			StringColumn("key", "UNIQUE").
+			TextColumn("token").
+			StringColumn("last_used").
+			ToSql()
+		if err != nil {
+			log.Fatalln("17 - failed on table creation: ", err)
+			return err
+		}
+		
+		_, err = db.Query(context.Background(), sql)
+		if err != nil {
+			log.Fatalln("18 - failed on table creation: ", err)
+			return err
+		}
+	}
+
 	// Jokesbapak2 table
 
 	// Check if table exists
 	var tableJokesExists bool
-	err := db.QueryRow(context.Background(), `SELECT EXISTS (
+	err = db.QueryRow(context.Background(), `SELECT EXISTS (
 		SELECT FROM information_schema.tables 
 		WHERE  table_schema = 'public'
 		AND    table_name   = 'jokesbapak2'
@@ -26,9 +58,11 @@ func Setup() error {
 	}
 
 	if !tableJokesExists {
-		sql, _, err := bob.CreateTable("jokesbapak2").
-			Columns("id", "link").
-			Types("SERIAL", "TEXT").
+		sql, _, err := bob.
+			CreateTable("jokesbapak2").
+			AddColumn(bob.ColumnDef{Name: "id", Type: "SERIAL", Extras: []string{"PRIMARY KEY"}}).
+			TextColumn("link", "UNIQUE").
+			AddColumn(bob.ColumnDef{Name: "creator", Type: "INT", Extras: []string{"NOT NULL", "REFERENCES \"administrators\" (\"id\")"}}).
 			ToSql()
 		if err != nil {
 			log.Fatalln("11 - failed on table creation: ", err)
@@ -40,70 +74,7 @@ func Setup() error {
 			log.Fatalln("12 - failed on table creation: ", err)
 			return err
 		}
-		_, err = db.Query(context.Background(), "ALTER TABLE \"jokesbapak2\" ADD creator INT NOT NULL DEFAULT 0;")
-		if err != nil {
-			log.Fatalln("13 - failed on table creation: ", err)
-			return err
-		}
-
-		_, err = db.Query(context.Background(), "ALTER TABLE \"jokesbapak2\" ADD PRIMARY KEY (\"id\")")
-		if err != nil {
-			log.Fatalln("14 - failed on table alteration: ", err)
-			return err
-		}
-
-		_, err = db.Query(context.Background(), "ALTER TABLE \"jokesbapak2\" ADD UNIQUE (\"link\")")
-		if err != nil {
-			log.Fatalln("15 - failed on table alteration: ", err)
-			return err
-		}
 	}
 
-	// administrators table
-	var tableAuthExists bool
-	err = db.QueryRow(context.Background(), `SELECT EXISTS (
-		SELECT FROM information_schema.tables 
-		WHERE  table_schema = 'public'
-		AND    table_name   = 'administrators'
-		);`).Scan(&tableAuthExists)
-	if err != nil {
-		log.Fatalln("16 - failed on checking table: ", err)
-		return err
-	}
-
-	if !tableAuthExists {
-		sql, _, err := bob.CreateTable("administrators").
-			Columns("id", "key", "token", "last_used").
-			Types("SERIAL", "VARCHAR(255)", "TEXT", "VARCHAR(255)").
-			ToSql()
-		if err != nil {
-			log.Fatalln("17 - failed on table creation: ", err)
-			return err
-		}
-
-		_, err = db.Query(context.Background(), sql)
-		if err != nil {
-			log.Fatalln("18 - failed on table creation: ", err)
-			return err
-		}
-
-		_, err = db.Query(context.Background(), "ALTER TABLE \"administrators\" ADD PRIMARY KEY (\"id\");")
-		if err != nil {
-			log.Fatalln("19 - failed on table alteration: ", err)
-			return err
-		}
-
-		_, err = db.Query(context.Background(), "ALTER TABLE \"administrators\" ADD UNIQUE (\"key\");")
-		if err != nil {
-			log.Fatalln("20 - failed on table alteration: ", err)
-			return err
-		}
-
-		_, err = db.Query(context.Background(), "ALTER TABLE \"jokesbapak2\" ADD CONSTRAINT fk_auth_key FOREIGN KEY (\"creator\") REFERENCES \"administrators\" (\"id\");")
-		if err != nil {
-			log.Fatalln("21 - failed on foreign key iteration: ", err)
-			return err
-		}
-	}
 	return nil
 }
--- api/go.mod
+++ api/go.mod
@@ -4,17 +4,17 @@ go 1.16
 
 require (
 	github.com/Masterminds/squirrel v1.5.0
-	github.com/aldy505/bob v0.0.1
+	github.com/aldy505/bob v0.0.2
 	github.com/aldy505/phc-crypto v1.1.0
 	github.com/allegro/bigcache/v2 v2.2.5 // indirect
 	github.com/allegro/bigcache/v3 v3.0.0
 	github.com/georgysavva/scany v0.2.9
 	github.com/getsentry/sentry-go v0.11.0
 	github.com/go-redis/redis/v8 v8.11.0
-	github.com/gofiber/fiber/v2 v2.14.0
+	github.com/gofiber/fiber/v2 v2.15.0
 	github.com/gojek/heimdall/v7 v7.0.2
-	github.com/jackc/pgx/v4 v4.11.0
+	github.com/jackc/pgx/v4 v4.12.0
 	github.com/joho/godotenv v1.3.0
 	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7
-	github.com/stretchr/testify v1.5.1
+	github.com/stretchr/testify v1.7.0
 )
